a2254c1e34e2cd45180b588900818421a598703c
Chadtech <chadstearns1@gmail.com>
2017-04-24 02:30:45 -0400

    Cleaned up main go file

--- source/main.go
+++ source/main.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/go-martini/martini"
 import "io/ioutil"
-import "fmt"
+// import "fmt"
 
 
 func main() {
@@ -13,15 +13,13 @@ func main() {
     m.Get("/", func() string {
         s, err := ioutil.ReadFile("./apps/test.html")
 
-        fmt.Println(err)
-
-        if (err == nil) {
+        if err == nil {
             return string(s)
         } else {
             return "dank memes"
         }
     })
 
-    
+
     m.Run()
 }
